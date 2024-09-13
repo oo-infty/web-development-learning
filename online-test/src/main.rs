3ffd3790337a6462a0dae193dc70d546cff214a3
oo-infty <oo-infty@outlook.com>
2024-09-13 09:29:22 +0800

    feat(online-test/backend): improve configuration loading

--- online-test/src/main.rs
+++ online-test/src/main.rs
@@ -1,31 +1,35 @@
 use std::sync::Arc;
 
+use online_test::domain::application::Core;
+use online_test::inbound::server::Server;
 use online_test::repository::connection::{AsyncSqliteConnectionManager, AsyncSqlitePool};
 use online_test::repository::question::QuestionSqliteRepository;
 use online_test::repository::score::ScoreSqliteRepository;
 use snafu::{prelude::*, Whatever};
 
-use online_test::domain::application::Core;
-use online_test::inbound::server::Server;
-
 #[tokio::main(flavor = "current_thread")]
 #[snafu::report]
 async fn main() -> Result<(), Whatever> {
-    let config = AsyncSqliteConnectionManager::new("production/data.db");
-    let database_pool: Arc<AsyncSqlitePool> = AsyncSqlitePool::builder(config)
+    dotenvy::dotenv().whatever_context("Could not read server configurations for .env")?;
+    let database_url = std::env::var("DATABASE_URL").unwrap_or("production/data.db".to_owned());
+    let listening_ip = std::env::var("LISTENING_IP").unwrap_or("0.0.0.0".to_owned());
+    let listening_port = std::env::var("LISTENING_PORT").unwrap_or("8080".to_owned());
+
+    let config = AsyncSqliteConnectionManager::new(&database_url);
+    let database_pool = AsyncSqlitePool::builder(config)
         .build()
-        .whatever_context("Could not initialize database pool")?
-        .into();
+        .map(|pool| Arc::new(pool))
+        .whatever_context("Could not initialize database pool")?;
 
     let question_repository = Arc::new(QuestionSqliteRepository::new(Arc::clone(&database_pool)));
     let score_repository = Arc::new(ScoreSqliteRepository::new(Arc::clone(&database_pool)));
 
     let core = Arc::new(Core::new(question_repository, score_repository));
 
-    let addr = std::env::var("SERVER_ADDR").unwrap_or("0.0.0.0:8080".to_owned());
-    Server::new(addr.parse().unwrap(), core)
+    let listening_addr = format!("{listening_ip}:{listening_port}");
+    Server::new(listening_addr.parse().unwrap(), core)
         .await
-        .unwrap()
+        .whatever_context("Could not initialize server")?
         .serve()
         .await
         .whatever_context("Server error occurred")?;
